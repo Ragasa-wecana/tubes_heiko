3477ce392e1a7110f575321e115d1ee6718969a3
unknown <wecanar@gmail.com>
2025-06-11 06:36:25 +0700

    revisi

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,12 +26,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 // tamnbahan
-<<<<<<< HEAD
-// use App\Filament\Widgets\BarangChart;
-=======
-use App\Filament\Widgets\BarangChart;
 
->>>>>>> 13e3c2132bbc18b8c9a8518d95e2b5ce74b13592
+use App\Filament\Widgets\BarangChart;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -68,18 +64,7 @@ class AdminPanelProvider extends PanelProvider
             ->pages([
                 Pages\Dashboard::class,
             ])
-<<<<<<< HEAD
-            //->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->widgets([
-                \App\Filament\Widgets\DashboardStatCards::class,
-                \App\Filament\Widgets\TotalPenjualanChart::class,
-                \App\Filament\Widgets\PenjualanPerBulanChart::class,
-                \App\Filament\Widgets\PenjualanPerBarangChart::class,
-                \App\Filament\Widgets\PieChartPenjualan::class,
-
-            ])
-=======
->>>>>>> 13e3c2132bbc18b8c9a8518d95e2b5ce74b13592
+            
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
